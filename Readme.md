99efcdbe394b247fdea2c8442b7d69b202ed8327
Chrioyles <chrioyles@gmx.de>
2018-09-06 18:58:02 +0200

    Add the source, readme

---
+++ Readme.md
@@ -0,0 +1,94 @@
+# EZStore
+## whats that?
+EZStore is a Javascript library that offers an easy to use store module.
+
+## how can i use it?
+```
+npm i --save EZStore
+```
+
+## what do i require?
+
+* Ecmascript 6
+* Spread Operator Module
+
+## how does it work?
+
+### import it
+
+```
+import EZStore from 'ezstore' 
+```
+
+### create a store
+
+```
+const myStore = new EZStore(
+  { buttonVisible: false,
+    isShiny: true
+  }
+);
+```
+
+### listen to changes on store keys
+```
+store.subscribe('myValue', (callBackValue) => {
+    // Do whatever you want when myValue changes
+    // You have access to the new value from the parameter of the callback function ( callBackValue )
+})
+```
+
+### get the value of a key in the store
+```
+store.get('myValue');
+```
+
+### set a new value to a key in the store
+```
+store.set('myValue', 'cheesecake');
+```
+
+## example snippet
+
+```
+import EZStore from 'ezstore';
+// Create the store
+const store = new EZStore({
+    buttonRendered: false,
+    timer: 5
+)}
+
+// Create a button
+let myButton = document.createElement('button');
+myButton.id = "myButton";
+
+// Create a timer
+let timer = document.createElement('div');
+timer.innerHTML = store.get('timer');
+document.body.appendChild(timer);
+
+// Listen to buttonRendered changes in store
+
+store.subscribe('buttonRendered', (value) => {
+    if(value === true) {
+        document.body.appendChild(myButton);
+    }
+})
+
+// Listen to timer changes in store
+store.subscribe('timer', (value) => {
+    timer.innerHTML = value;
+})
+
+// Init a counter that counts from 5 to 0
+const counter = setInterval(() => {
+    store.set('timer', (store.get('timer') -1 )
+}, 1000)
+
+// Init a Timeout that displays a button after 5 seconds
+setTimeout(() => {
+    store.set('timer', '');
+    store.set('buttonRendered': true);
+}, 5000)
+
+```
---
+++ index.js
@@ -0,0 +1,98 @@
+/**
+* Author: Chrioyles
+* Date: 06.09.2018
+**/
+
+class EZ_Store {
+  /**
+  * PUBLIC FUNCTIONS
+  **/
+
+  /**
+  * Create the store while filling it with data
+  **/
+  constructor(storeObject){
+    if(!typeof storeObject === 'object'){
+      console.warn(
+        'Error in module EZ_Store. Object passed to create function has to be' +
+        'typeof Object.'
+      );
+      return {};
+    }
+    this._data = {...storeObject};
+    return this;
+  }
+
+  /**
+  * Set a value to a key in the store. Key must match a key in the _data Object.
+  **/
+  set(key, value){
+    if(!(typeof key === 'string') ||
+       !this._data.hasOwnProperty(key) ||
+      (value === this_data[key])) return;
+    this._data[key] = value
+    this._dispatchChange(this._listeningOn(key))
+  }
+
+  /**
+  * Get a value of a key in the store. Key must match a key in the _data Object.
+  **/
+  get(key, value){
+    if(!(typeof key === 'string') ||
+       !this._data.hasOwnProperty(key) return;
+    return this._data[key];
+  }
+
+  /**
+  * Subscribe to a key in the store. Triggers a change when the value of the key
+  * changes
+  **/
+  subscribe(...parameter) {
+    const functionsInParameter = parameter.filter(
+      parameter => typeof parameter === 'function'
+    )
+    const cb = (functionsInParameter.length > 0) ? functionsInParameter[0] : null;
+    if(!cb) return;
+
+    const stringsInParameter = parameter.filter(
+      parameter => typeof parameter === 'string'
+    )
+    const key = (stringsInParameter.length > 0) ? stringsInParameter[0] : null;
+    if(!key) return;
+
+    const listener = {};
+    listener[key] = cb;
+    this.listeners.push(listener);
+  }
+
+  /**
+  * PRIVATE FUNCTIONS
+  **/
+
+  /**
+  * Filters out all subscriber function to the key specified as a parameter.
+  **/
+  _listeningOn(key) {
+    if(!typeof key === 'string') return;
+    return this.listeners.filter(
+      listener => listener.hasOwnProperty(key)
+    )
+  }
+
+  /**
+  * Calls all functions of the listeners specified in the parameters.
+  **/
+  _dispatchChange(listeners){
+    const that = this;
+    listeners.forEach(
+      listener => {
+        const keyName = Object.keys(listener)[0];
+        if (keyName){
+          listener[keyName](that._data[keyName])]
+        }
+      }
+    )
+  }
+}
+
+export default EZ_Store;
