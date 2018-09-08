da721f5fc14a716552ae30f66d571a8d5043f1ce
Chrioyles <chrioyles@gmx.de>
2018-09-08 12:52:20 +0200

    Move index build to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'build/'),
+        path: path.resolve(__dirname, '.'),
         filename: 'index.js'
     },
     resolve: {
